f8111d543db80e9b10f2ef632cce9fad59424e14
IlahaBashirova <11861@holbertonstudents.com>
2026-02-19 14:50:57 +0400

    Add pharmacies search/id endpoints + update swagger

--- backend/src/routes/pharmacy.routes.js
+++ backend/src/routes/pharmacy.routes.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const pharmacyController = require("../controllers/pharmacy.controller");
 
 router.get("/", pharmacyController.getAll);
+router.get("/search", pharmacyController.search);
+router.get("/:id", pharmacyController.getById);
 
 module.exports = router;
